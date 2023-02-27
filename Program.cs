713808fc482e45f05e424ad0cdd901010feadee8
Gimena Medrano <fmedranop1@miumg.edu.gt>
2023-02-27 14:23:16 -0600

    Codigo actualizado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string clave = "abc", pwd;
+int intentos = 3;
+
+do
+{
+    Console.WriteLine("Hola, ingresa tu contraseña");
+    pwd = Console.ReadLine();
+
+    if (clave.Equals(pwd))
+    {
+        Console.WriteLine("contraseña correcta");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("contraseña incorrecta");
+        intentos--;
+        Console.WriteLine($"te quedan {intentos} intentos");
+
+    }
+
+} while (intentos > 0 || !clave.Equals(pwd));
